a223da3239928330e5373af276d810212b9f9718
Chetan Chauhan <chetanchauhan1394@gmail.com>
2021-11-13 17:17:58 +0530

    first commit

---
+++ src/app/message.service.ts
@@ -0,0 +1,21 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
+
+  constructor() {}
+
+  add(message: string) {
+    console.log("messages==>", this.messages);
+    
+    this.messages.push(message);
+  }
+
+  clear(){
+    this.messages = [];
+  }
+}
